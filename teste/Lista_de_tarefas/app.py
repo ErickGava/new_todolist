19b7661d56be8da1339a9c50f3c0316ae23c1822
ErickGava <erickalessigava@gmail.com>
2025-03-28 17:14:44 -0300

    cadastro

--- teste/Lista_de_tarefas/app.py
+++ teste/Lista_de_tarefas/app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+import database
 
 app = Flask (__name__)
 
@@ -10,9 +14,19 @@ def hello ():
 def login ():
     return render_template ('login.html')
 
-@app.route ('/cadastro')
+@app.route ('/cadastro', methods=["GET", "POST"])
 def cadastro ():
-    return render_template ('cadastro.html')
+    if request.method == "POST":
+        form = request.form
+    
+        if database.criar_usuario(form) == True:
+            return render_template('login.html')
+
+        else:
+            return "Ocorreu um erro ao cadastrar o usuário!"
+    
+    else:   
+        return render_template ('cadastro.html')
 
 if __name__ == '__main__':
     app.run (debug = True)
--- teste/Lista_de_tarefas/database.py
+++ teste/Lista_de_tarefas/database.py
@@ -10,6 +10,23 @@ def criar_tabelas ():
     cursor.execute ('''create table if not exists usuarios (email text primary key, nome text, senha text)''')
     cursor.execute ('''create table if not exists tarefas (id integer primary key, conteudo text, esta_conculida integer, email_usuario text, foreign key(email_usuarios) references usuarios(email))''')
     conexao.commit ()
+    
+def criar_usuario(formulario):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+    cursor.execute('''SELECT count(email) FROM usuarios WHERE email=?''', (formulario['email'],))
+    conexao.commit()
+    
+    quantidade_de_emails_cadastrados = cursor.fetchone()
+    if(quantidade_de_emails_cadastrados[0] > 0):
+        return False
+    
+    cursor.execute('''INSERT INTO usuarios (email, nome, senha)
+                    VALUES(?,?,?)''',
+                    (formulario['email'], formulario['nome'], formulario['senha']))
+    conexao.commit()
+    
+    return True
 
 if __name__ == "__main__":
     criar_tabelas ()
